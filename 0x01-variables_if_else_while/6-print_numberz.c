a1b2202e6bd5a0faabfa398a851097693fef3263
Emmanuel <emmybike11@gmail.com>
2022-06-03 17:29:28 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * @0 - Return value
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}	
