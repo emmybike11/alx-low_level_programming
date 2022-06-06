95bf95e15e24ddedc9fcd360b767d772b3f33312
Emmanuel <emmybike11@gmail.com>
2022-06-06 13:46:14 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- The function
+ * @0- The Return
+ * Return: Thats cool
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0):
+}
