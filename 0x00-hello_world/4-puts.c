6c60e8ab39ad31c8c734bacc2cdd3ffbbdbd26bd
Emmanuel <emmybike11@gmail.com>
2022-06-06 13:40:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- i dont want to say anything
+ * @0 - haha you are stressing me
+ * return : 0 you get
+ *
+ * /
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
